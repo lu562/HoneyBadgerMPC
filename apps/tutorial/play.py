0072e50b9f9d4eac70406476e558a142eca5ee78
lu562 <lu562@purdue.edu>
2021-03-07 21:54:22 -0800

    t

--- apps/tutorial/play.py
+++ apps/tutorial/play.py
@@ -331,7 +331,7 @@ async def run(ctx, **kwargs):
     # # logging.info(f"A*B:{AtimesB} A<B:{AltB} B<A:{BltA}")
     # logging.info("Finished _prog")
 
-    k = 2500
+    k = 3000
     # Our method
     rs = [ctx.Share(10)] * k
     rs_msb = [ctx.Share(0)] * k
